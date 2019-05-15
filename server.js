c77e53fc82b3a976eb1ac893786f9f0eb4346150
Sekoya Hicks <sekoya91@yahoo.com>
2019-05-15 12:32:55 -0400

    Deploying

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
-const express = require('express')
-const app = express()
-const methodOverride = require('method-override')
-const  = require('.excuses_excuses/todo.js')
-const userApi = require('./api/userApi.js')
+const express = require('express');
+const app = express();
 
-// Register middleware...
+app.use(express.json());
+app.get('/', (req,res) => {
+    res.send('Hello world!')
+})
 
-//...for decoding body as js object
-app.use(express.urlencoded())
-
-//...for "faking" DELETE and PUT/PATCH requests
-app.use(methodOverride('_method'))
-
-//add middleware for handlebars here
-app.set('view engine', 'hbs')
-app.use(express.static(__dirname+"/public"))
-
-app.get("/users", (req, res) => {
-  userApi.getUsers()
-    .then(users => {
-      res.render('users/users', { users });
-    });
-});
-//keep these lines at the bottom of the file
-const PORT = process.env.PORT || 3000 
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`App is listening on PORT ${PORT}`)
+    console.log("Magic happening on port " + PORT);
 })
